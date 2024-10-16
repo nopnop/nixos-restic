0693fc85e24ddce59ca58da56b45362952a213d9
Jean Ponchon <jean.ponchon@novainsilico.ai>
2024-10-16 19:35:11 +0200

    feat: add bash history to backup

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 echo "Backup started at $(date)"
 
-restic backup --verbose ~/Documents ~/.config/google-chrome ~/.local/share/DBeaverData  \
+restic backup --verbose ~/Documents ~/.config/google-chrome ~/.local/share/DBeaverData ~/.bash_history  \
   --exclude .cache \
   --exclude node_modules \
   --skip-if-unchanged
