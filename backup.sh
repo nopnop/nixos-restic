0f018fab1e12a92397db18023bbb57fb49af6ad7
Jean Ponchon <jean.ponchon@novainsilico.ai>
2024-10-16 18:39:59 +0200

    fix: add more folders to backup

--- backup.sh
+++ backup.sh
@@ -2,7 +2,8 @@
 
 echo "Backup started at $(date)"
 
-restic backup --verbose ~/Documents \
+restic backup --verbose ~/Documents ~/.config/google-chrome ~/.local/share/DBeaverData  \
   --exclude .cache \
   --exclude node_modules \
   --skip-if-unchanged
+
