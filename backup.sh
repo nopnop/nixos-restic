68ff1460ce59d200877355bf85b35fba6cc50aa3
Jean Ponchon <jean.ponchon@novainsilico.ai>
2024-11-18 16:57:12 +0100

    exclude qcow2 files

--- backup.sh
+++ backup.sh
@@ -5,5 +5,6 @@ echo "Backup started at $(date)"
 restic backup --verbose ~/Documents ~/.config/google-chrome ~/.local/share/DBeaverData ~/.bash_history  \
   --exclude .cache \
   --exclude node_modules \
+  --exclude '*.qcow2' \
   --skip-if-unchanged
 
